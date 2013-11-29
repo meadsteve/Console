8130ea2dc5cd3e28e0f2043d11218c632a4683fb
meadsteve <meadsteve@gmail.com>
2013-11-29 18:52:01 +0000

    update example.

--- example.php
+++ example.php
@@ -4,5 +4,9 @@ require_once __DIR__ .  "/vendor/autoload.php";
 $shell = new MeadSteve\Console\Shell();
 
 $gitStatus = $shell->newCommand('git')->addArg('status')->execute();
+echo implode("\n", $gitStatus);
 
+echo PHP_EOL . PHP_EOL . "And Another way:" . PHP_EOL . PHP_EOL;
+
+$gitStatus = $shell->executeCommand('git', array('status'));
 echo implode("\n", $gitStatus);
